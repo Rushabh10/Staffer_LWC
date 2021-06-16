4bd78ab099e12ef9af19f807a288de909fd275d3
Rushabh Musthyala <rmusthyala@rmusthy-ltmcjvg.internal.salesforce.com>
2021-06-16 17:23:31 +0530

    UI changes

--- force-app/main/default/lwc/stafferDataTable/stafferDataTable.js
+++ force-app/main/default/lwc/stafferDataTable/stafferDataTable.js
@@ -7,7 +7,8 @@ import STAFFER_FORMER from '@salesforce/schema/Staffer__c.Former__c';
 import STAFFER_CONTACT from '@salesforce/schema/Staffer__c.Contact__c';
 import { NavigationMixin } from 'lightning/navigation';  
 import { deleteRecord } from 'lightning/uiRecordApi';
-
+import { refreshApex } from '@salesforce/apex';
+import getAllStaffers2 from '@salesforce/apex/getStaffersLWC.getAllStaffers2';
 
 export default class StafferDataTable extends NavigationMixin(LightningElement) {
     @track data;
@@ -15,19 +16,21 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
     @track columns = [ { label: 'Name', fieldName: 'Name', type: "text", sortable: "true"},
                     { label: 'Phone Number', fieldName: 'Phone_Number__c',type: "phone", sortable: "true"},
                     { label: 'Former', fieldName: 'Former__c', type: 'checkbox', sortable: "false"},
-                    { type: "button", initialWidth: 75, typeAttributes: {
+                    { type: "button-icon", fixedWidth: 50, typeAttributes: {
                         name: 'Edit',
                         title: 'Edit',
                         disabled: false,
+                        variant: 'bare',
                         value: 'edit',
                         iconName: 'utility:edit',
                         iconPosition: 'center'
                     }},
-                    { type: "button", initialWidth: 75, typeAttributes: {
+                    { type: "button-icon", fixedWidth: 50, typeAttributes: {
                         name: 'Delete',
                         title: 'Delete',
                         disabled: false,
                         value: 'delete',
+                        variant: 'bare',
                         iconName: 'utility:delete',
                         iconPosition: 'center'
                     }}];
@@ -51,7 +54,7 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
     @api recordId;
     @track draftValues = [];
     @track recvData;
-    @api objectApiName;
+    @api objectApiName = "Staffer__c";
 
     @track stafferName = STAFFER_NAME;
     @track stafferPhone = STAFFER_PHONE;
@@ -65,29 +68,40 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
     @track formerEmpty = false;
     @track openModalNew = false;
     @track deleteModal = false;
+    @track wiredResults;
 
-    @wire(getAllStaffers, {recId: '$recordId'}) stafferRecords({error, data}) {
-        if(data) {
-            console.log(data);
-            this.searchedData_f = data[1];
+    @wire(getAllStaffers, {recId: '$recordId'}) stafferRecords(result) {
+        this.wiredResults = result;
+        const { data, error } = result;
+        if(result.data) {
+            this.data = data;
+            console.log(this.data);
+            this.searchedData_f = this.data[1];
             console.log("Got former staffers");
-            this.allData_f = data[1];
-            this.totalRecountCount_f = data[1].length;
+            this.allData_f = this.data[1];
+            this.totalRecountCount_f = this.data[1].length;
             this.totalPage_f = Math.ceil(this.totalRecountCount_f / this.pageSize);
+            if(this.totalPage_f == 0) {
+                this.page_f = 0;
+            }
             this.data_f = this.allData_f.slice(0, this.pageSize);
             this.endingRecord_f = this.pageSize;
             console.log("Got staffers");
-            this.searchedData = data[0];
-            this.recvData = data[0];
-            this.allData = data[0];
-            this.totalRecountCount = data[0].length;
+            this.searchedData = this.data[0];
+            this.recvData = this.data[0];
+            this.allData = this.data[0];
+            this.totalRecountCount = this.data[0].length;
             this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize);
+            if(this.totalPage == 0) {
+                this.page = 0;
+            }
             this.data = this.allData.slice(0, this.pageSize);
             this.endingRecord = this.pageSize;
            // this.stafferId = data[0][0].Id;
         }
         else if(error) {
             this.data = undefined;
+            return refreshApex(this.wiredResults);
         }
     }
 
@@ -301,16 +315,21 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
             {
                 if(this.stafferId !== null) {
                     this.template.querySelector('lightning-record-form').submit(fields);
-                    this.dispatchEvent(new ShowToastEvent({
+                    this.dispatchEvent(
+                        new ShowToastEvent({
                         title: 'Success',
                         message: 'Changes saved.',
                         variant: 'success',
                     }),
-                    );
+                    );   
+                    this.openModal = false;
+                    this.openModalNew = false;
+                    return refreshApex(getAllStaffers2);
                 }
                 this.openModal = false;
                 this.openModalNew = false;
-                //location.reload();
+                // this.refreshData();
+                // console.log('refresh apex done');
             }
             else {
                 if(this.stafferId !== null) {
@@ -348,7 +367,7 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
             );
             this.openModal = false;
             this.deleteModal = false;
-            location.reload();
+            this.refreshData();
         });
     }
 
@@ -372,6 +391,10 @@ export default class StafferDataTable extends NavigationMixin(LightningElement) 
             this.stafferId = event.detail.row.Id;
             this.deleteModal = true;
         }
+    }
+
+    refreshData() {
+        return refreshApex(this.wiredResults);
     }
 }
 
